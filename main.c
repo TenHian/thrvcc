9377d1b297be69398e6ae94eb51663e0f14785a8
TenHian <tenhian@outlook.com>
2023-04-17 15:23:51 +0800

    [4] Supported () * /

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
+#include <assert.h>
+#include <ctype.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include <ctype.h>
 
-static char *InputArgv;
+static char *InputArgv;		// reg the argv[1]
+static int StackDepth;
 
 enum TokenKind {
 	TK_PUNCT, // operator like "+" "-"
@@ -31,7 +33,8 @@ static void error_out(char *fmt, ...)
 	exit(1);
 }
 
-static void verror_at(char *location, char *fmt, va_list va) {
+static void verror_at(char *location, char *fmt, va_list va)
+{
 	fprintf(stderr, "%s\n", InputArgv);
 
 	int pos = location - InputArgv;
@@ -42,14 +45,16 @@ static void verror_at(char *location, char *fmt, va_list va) {
 	va_end(va);
 }
 
-static void error_at(char *location, char *fmt, ...) {
+static void error_at(char *location, char *fmt, ...)
+{
 	va_list va;
 	va_start(va, fmt);
 	verror_at(location, fmt, va);
 	exit(1);
 }
 
-static void error_token(struct Token *token, char *fmt, ...) {
+static void error_token(struct Token *token, char *fmt, ...)
+{
 	va_list va;
 	va_start(va, fmt);
 	verror_at(token->location, fmt, va);
@@ -110,7 +115,7 @@ static struct Token *token_parser()
 		}
 
 		// parse operator
-		if (*formula == '+' || *formula == '-') {
+		if (ispunct(*formula)) {
 			cur->next = new_token(TK_PUNCT, formula, formula + 1);
 			cur = cur->next;
 			++formula;
@@ -124,6 +129,168 @@ static struct Token *token_parser()
 	return head.next;
 }
 
+/*
+ * generate AST, Implementing a syntax parser
+*/
+
+// def AST node's kind
+enum NodeKind {
+	ND_ADD,
+	ND_SUB,
+	ND_DIV,
+	ND_MUL,
+	ND_NUM,
+};
+
+struct AstNode {
+	enum NodeKind kind;
+	struct AstNode *lhs;
+	struct AstNode *rhs;
+	int val;
+};
+
+static struct AstNode *new_astnode(enum NodeKind kind)
+{
+	struct AstNode *node = calloc(1, sizeof(struct AstNode));
+	node->kind = kind;
+	return node;
+}
+
+static struct AstNode *new_binary_tree_node(enum NodeKind kind,
+					    struct AstNode *lhs,
+					    struct AstNode *rhs)
+{
+	struct AstNode *node = new_astnode(kind);
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+static struct AstNode *new_num_astnode(int val)
+{
+	struct AstNode *node = new_astnode(ND_NUM);
+	node->val = val;
+	return node;
+}
+
+static struct AstNode *expr(struct Token **rest, struct Token *token);
+static struct AstNode *mul(struct Token **rest, struct Token *token);
+static struct AstNode *primary(struct Token **rest, struct Token *token);
+
+// expr = mul ("+" mul | "-" mul)*
+static struct AstNode *expr(struct Token **rest, struct Token *token)
+{
+	struct AstNode *node = mul(&token, token);
+
+	while (true) {
+		if (equal(token, "+")) {
+			node = new_binary_tree_node(ND_ADD, node,
+						    mul(&token, token->next));
+			continue;
+		}
+		if(equal(token, "-")) {
+			node = new_binary_tree_node(ND_SUB, node,
+						    mul(&token, token->next));
+			continue ;
+		}
+		*rest = token;
+		return node;
+	}
+}
+// mul = primary ("*" primary | "/" primary) *
+static struct AstNode *mul(struct Token **rest, struct Token *token)
+{
+	struct AstNode *node = primary(&token, token);
+
+	while (true) {
+		if(equal(token, "*")) {
+			node = new_binary_tree_node(ND_MUL, node, primary(&token, token->next));
+			continue ;
+		}
+		if(equal(token, "/")) {
+			node = new_binary_tree_node(ND_DIV, node, primary(&token, token->next));
+			continue ;
+		}
+
+		*rest = token;
+		return node;
+	}
+}
+
+// parse "(" ")" and num
+static struct AstNode *primary(struct Token **rest, struct Token *token)
+{
+	// "(" expr ")"
+	if(equal(token, "(")) {
+		struct AstNode *node = expr(&token, token->next);
+		*rest = skip(token, ")");
+		return node;
+	}
+	// num
+	if(token->kind == TK_NUM) {
+		struct AstNode *node = new_num_astnode(token->val);
+		*rest = token->next;
+		return node;
+	}
+
+	error_token(token, "expected an expression");
+	return NULL;
+}
+
+// push stack, mov the tem result into stack
+// *sp* is stack pointer, stack grows down, 8 bytes to a unit at 64 bits mode
+// *sp* point to cur stack, so, push a0 into stack
+// the reason for not using register storage is because \
+// the number of values to be stored is variable
+static void push(void)
+{
+	printf("  addi sp, sp, -8\n");
+	printf("  sd a0, 0(sp)\n");
+	StackDepth++;
+}
+
+// pop stack, mov the top val of stack into a1
+static void pop(char *reg)
+{
+	printf("  ld %s, 0(sp)\n", reg);
+	printf("  addi sp, sp, 8\n");
+	StackDepth--;
+}
+
+static void gen_expr(struct AstNode *node)
+{
+	if(node->kind == ND_NUM) {
+		printf("  li a0, %d\n", node->val);
+		return ;
+	}
+
+	gen_expr(node->rhs);
+	push();
+	gen_expr(node->lhs);
+	pop("a1");
+
+	// gen bin-tree node
+	switch (node->kind) {
+	case ND_ADD: // + a0=a0+a1
+		printf("  add a0, a0, a1\n");
+		return ;
+	case ND_SUB: // - a0=a0-a1
+		printf("  sub a0, a0, a1\n");
+		return ;
+	case ND_MUL: // * a0=a0*a1
+		printf("  mul a0, a0, a1\n");
+		return ;
+	case ND_DIV: // / a0=a0/a1
+		printf("  div a0, a0, a1\n");
+		return ;
+	default:
+		break ;
+	}
+
+	error_out("invalid expression");
+}
+
+
 int main(int argc, char *argv[])
 {
 	if (argc != 2) {
@@ -131,31 +298,23 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
+	// parse argv[1], gen token stream
 	InputArgv = argv[1];
 	struct Token *token = token_parser(argv[1]);
 
+	// parse token stream
+	struct AstNode *ast_node = expr(&token, token);
+	if(token->kind != TK_EOF)
+		error_token(token, "extra token");
+
 	printf("  .globl main\n");
 	printf("main:\n");
-	// parse first token, digit
-	printf("  li a0, %d\n", get_number(token));
-	token = token->next;
-
-	// parse (opt + num)
-	while (token->kind != TK_EOF) {
-		if (equal(token, "+")) {
-			token = token->next;
-			printf("  addi a0, a0, %d\n", get_number(token));
-			token = token->next;
-			continue;
-		}
-		if (equal(token, "-")) {
-			token = token->next;
-			printf("  addi a0, a0, -%d\n", get_number(token));
-			token = token->next;
-		}
-	}
-
+	// iterate through the AST tree to generate assembly code
+	gen_expr(ast_node);
 	printf("  ret\n");
+
+	// if stack is not empty, error
+	assert(StackDepth == 0);
 
 	return 0;
 }
--- test.sh
+++ test.sh
@@ -18,12 +18,20 @@ assert() {
 }
 
 # [1] return the input val
+echo [1]
 assert 0 0
 assert 42 42
 # [2] support "+, -" operation
+echo [2]
 assert 34 '12-34+56'
 # [3] support whitespaces
+echo [3]
 assert 41 ' 12 + 34 - 5 '
+# [5] support * / ()
+echo [5]
+assert 47 '5+6*7'
+assert 15 '5*(9-6)'
+assert 17 '1-8/(2*2)+3*6'
 
 # if all fine, echo OK
 echo OK
